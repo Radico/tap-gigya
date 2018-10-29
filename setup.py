f72d2dd8a34f81ca94db95c00c229071ccbee1d8
Cody Schwarz <cody@simondata.com>
2018-10-29 12:33:19 -0400

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-chatitive",
-    version="0.1.0",
+    version="0.1.1",
     description="Singer.io tap for extracting data",
     author="Simon Data",
     url="http://simondata.com",
