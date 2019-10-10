66bf84a2fb1f8d5d5d90e414e37f3ef6c309101b
Asher Woodbury <asher@Ashers-MacBook-Pro.local>
2019-10-10 17:19:12 -0400

    Changing tap kit accessor:

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setup(
         "singer-python==5.2.0",
         'requests==2.18.4',
         "pendulum==1.2.0",
-        "tap-kit==0.1.1",
-        "ipdb==0.8.1"
+        "ipdb==0.8.1",
+        "tap-kit @ git+https://github.com/Radico/tap-kit.git@master"
     ],
     dependency_links=[
         "https://github.com/Radico/tap-kit/tarball/master#egg=tap-kit-0.1.1",
